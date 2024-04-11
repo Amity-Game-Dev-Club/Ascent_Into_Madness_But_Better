3c4826902c21f40e9be60a1d8350357829b79d6e
Angel Irigo <irigoyenan24@amityschools.org>
2024-04-11 14:46:06 -0400

    health manager

---
+++ Scripts/playerHealthManager.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var max_health : int = 3
+var current_health : int
+
+func _ready():
+	current_health = max_health
+
+
+func decrease_health(health_amount : int):
+	current_health -= health_amount
+	
+	if current_health < 0:
+		current_health = 0
+
+func increase_health(health_amount : int):
+	current_health += health_amount
+	
+	if current_health > max_health:
+		current_health = max_health
