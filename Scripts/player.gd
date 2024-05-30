513fe380cd67fcf71baefbecaf70d15498b653e9
real <mineyminergaming@gmail.com>
2024-05-30 14:33:42 -0400

    skbiiti

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,12 +4,15 @@ const JUMP_VELOCITY = -400.0
 var doublejump = 1
 var max_health : int = 3
 var current_health : int = 3
+@onready var animation = $AnimatedSprite2D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var bullet = preload("res://Scenes/buller.tscn")
 func _ready():
 	pass
 func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("right"):
+		animation.play("Run")
 	var direction = Input.get_axis("left", "right")
 	if not is_on_floor():
 		velocity.y += gravity * delta
